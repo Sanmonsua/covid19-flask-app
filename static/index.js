064df7aa76f5382f03aab03264a647bc7221934b
Sanmonsua <sanmonsua@gmail.com>
2020-09-30 17:12:10 -0500

    refactors JavaScript code and html

--- static/index.js
+++ static/index.js
@@ -1,69 +1,73 @@
-document.addEventListener('DOMContentLoaded', () =>{
-  const request = new XMLHttpRequest();
-  request.open('GET', '/global');
+document.addEventListener('DOMContentLoaded', function onDocumentLoaded() {
+	var request = new XMLHttpRequest()
+	request.open('GET', '/global')
 
-  request.onload = () =>{
-    const data = JSON.parse(request.responseText);
-    const confirmed = data.confirmed;
-    document.querySelector('#confirmed').innerHTML = confirmed;
-    const deaths = data.deaths;
-    document.querySelector('#deaths').innerHTML = deaths;
-    const recovered = data.recovered;
-    document.querySelector('#recovered').innerHTML = recovered;
-    var select = document.querySelector('#countrySelect')
-    for (var i = 0; i < data.countries.length; i++) {
-      const country = data.countries[i];
-      const opt = document.createElement('option');
-      opt.appendChild( document.createTextNode(country.Name) );
-      opt.value = country.Code;
-      select.appendChild(opt);
-    }
-  }
-  request.send();
+	request.onload = function onLoad() {
+		var { confirmed, deaths, recovered, countries } = JSON.parse(
+			request.responseText
+		)
+		document.querySelector('#confirmed').innerHTML = confirmed
+		document.querySelector('#deaths').innerHTML = deaths
+		document.querySelector('#recovered').innerHTML = recovered
+		var select = document.querySelector('#countrySelect')
+		countries.forEach(appendCountryToSelect)
 
-  document.querySelector('#form-country').onsubmit = () =>{
-    const request_country = new XMLHttpRequest();
-    request_country.open('POST', '/country-data');
+		function appendCountryToSelect({ Name, Code }) {
+			var opt = document.createElement('option')
+			opt.appendChild(document.createTextNode(Name))
+			opt.value = Code
+			select.appendChild(opt)
+		}
+	}
+	request.send()
 
-    request_country.onload = () =>{
-      const data = JSON.parse(request_country.responseText);
-      const results_cards = document.createElement('div');
-      results_cards.setAttribute("style", "margin-top:100px;");
-      results_cards.setAttribute("class", "card-deck col-12 align-middle");
-      results_cards.appendChild(createCard("bg-primary", "Confirmed", data.confirmed));
-      results_cards.appendChild(createCard("bg-danger", "Deaths", data.deaths));
-      results_cards.appendChild(createCard("bg-success", "Recovered", data.recovered));
-      document.querySelector('#results').appendChild(results_cards);
-    }
+	document.querySelector('#form-country').onsubmit = function onSubmit() {
+		var request_country = new XMLHttpRequest()
+		request_country.open('POST', '/country-data')
 
+		request_country.onload = () => {
+			var { confirmed, deaths, recovered } = JSON.parse(
+				request_country.responseText
+			)
+			var results_cards = document.createElement('div')
+			results_cards.setAttribute('style', 'margin-top:100px;')
+			results_cards.setAttribute('class', 'card-deck col-12 align-middle')
+			results_cards.appendChild(
+				createCard('bg-primary', 'Confirmed', confirmed)
+			)
+			results_cards.appendChild(createCard('bg-danger', 'Deaths', deaths))
+			results_cards.appendChild(
+				createCard('bg-success', 'Recovered', recovered)
+			)
+			document.querySelector('#results').appendChild(results_cards)
+		}
 
-    const country_select = document.querySelector('#countrySelect');
-    const opt = country_select.options[countrySelect.selectedIndex];
-    const data = new FormData();
-    data.append('country', opt.value);
-    request_country.send(data);
-    return false;
-  };
+		var countrySelect = document.querySelector('#countrySelect')
+		var { value } = country_select.options[countrySelect.selectedIndex]
+		var data = new FormData()
+		data.append('country', value)
+		request_country.send(data)
+		return false
+	}
+})
 
-});
-
-function createCard(class_card, heading, value) {
-  const card = document.createElement('div');
-  card.setAttribute("style", "max-width: 18rem;");
-  card.setAttribute("class", `card text-white ${class_card} mb-3`);
-  const header = document.createElement('div');
-  header.setAttribute("class", "card-header");
-  const header_value = document.createElement('h4');
-  header_value.setAttribute("class", "text-center")
-  header_value.innerHTML = heading;
-  header.appendChild(header_value);
-  card.appendChild(header);
-  const body = document.createElement('div');
-  body.setAttribute("class", "card-body");
-  const body_value = document.createElement('h1');
-  body_value.setAttribute("class", "text-center");
-  body_value.innerHTML = value;
-  body.appendChild(body_value);
-  card.appendChild(body);
-  return card;
+function createCard(classCard, heading, value) {
+	var card = document.createElement('div')
+	card.setAttribute('style', 'max-width: 18rem;')
+	card.setAttribute('class', `card text-white ${classCard} mb-3`)
+	var header = document.createElement('div')
+	header.setAttribute('class', 'card-header')
+	var header_value = document.createElement('h4')
+	header_value.setAttribute('class', 'text-center')
+	header_value.innerHTML = heading
+	header.appendChild(header_value)
+	card.appendChild(header)
+	var body = document.createElement('div')
+	body.setAttribute('class', 'card-body')
+	var body_value = document.createElement('h1')
+	body_value.setAttribute('class', 'text-center')
+	body_value.innerHTML = value
+	body.appendChild(body_value)
+	card.appendChild(body)
+	return card
 }
